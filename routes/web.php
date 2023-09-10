a881ec27756cd2d753a127fc2a2ef0a51e925703
clayping <cf@live.jp>
2023-09-10 10:25:33 +0900

    カレンダー処理実装

--- routes/web.php
+++ routes/web.php
@@ -35,5 +35,6 @@ Route::middleware([
         return view('full-calendar');
     })->name('calendar');
     Route::get('/calendar/action', [FullCalendarController::class, 'index']);
+    Route::post('/calendar/action', [FullCalendarController::class, 'action']);
 });
 
